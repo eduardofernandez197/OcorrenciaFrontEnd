60252d65c29e4840b069339ac2d95ce4d0614973
JamesGVidal <jamesgamavidal@gmail.com>
2026-06-15 16:04:57 -0300

    criacao_nova_ocorrencia_base

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ import './App.css'
 function App() {
 
     const [sideBarOpen, setSideBarOpen] = useState<boolean>(false)
+    const [newOcorrencia, setNewOcorrencia] = useState<boolean>(false)
 
     function sideBarAbre() { 
         setSideBarOpen ((prevState) => !prevState)
+    }
+
+    function createOcorrencia() {
+        setNewOcorrencia((prevState) => !prevState)
     }
 
     return(
@@ -16,7 +21,15 @@ function App() {
                 <button onClick={sideBarAbre}>Menu</button>
             </div>
             <div className={sideBarOpen ? "sidebar abre" : "sidebar fecha"}>
-                <div>Novas Ocorrencias</div>
+                <div>
+                    <button onClick={createOcorrencia}>Nova Ocorrência</button>
+                </div>
+            </div>
+            <div className={newOcorrencia ? "newOcorrencia abre" : "newOcorrencia fecha"}>
+                <form>
+                    <label htmlFor="">Titulo</label>
+                    <input name='titulo' type="text" id='titulo' />
+                </form>
             </div>
         </main>
         
