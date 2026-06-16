5fe4e275c473f6264c78f857784f90786a965320
JamesGVidal <jamesgamavidal@gmail.com>
2026-06-15 23:33:21 -0300

    Envia_dados_para_a_api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useState, type SyntheticEvent } from 'react'
+import { useState } from 'react'
+import { useForm } from "react-hook-form";
+import type { SubmitHandler } from 'react-hook-form';
 import './App.css'
+import axios from 'axios';
 
 
 function App() {
 
+    type FormValues = {
+        title: string
+        description: string
+        fotos?: FileList
+    }
+
     const [sideBarOpen, setSideBarOpen] = useState<boolean>(false)
     const [newOcorrencia, setNewOcorrencia] = useState<boolean>(false)
+    const {register, handleSubmit} = useForm<FormValues>()
 
     function sideBarAbre() { 
         setSideBarOpen ((prevState) => !prevState)
@@ -14,6 +24,23 @@ function App() {
     function createOcorrencia() {
         setNewOcorrencia((prevState) => !prevState)
     }
+
+    function onSubmit(data: FormValues) {
+        const formData = new FormData()
+        formData.append("title", data.title)
+        formData.append("description", data.description)
+        if (data.fotos) {
+            Array.from(data.fotos).forEach(file => formData.append("fotos", file))
+        }
+
+        fetchDados(formData)
+    }
+
+    async function fetchDados(formData: FormData) {
+        const response = await axios.post("http://localhost:8080/ocorrencias", formData)
+        console.log("Resposta da API:", response.data)
+    }
+
 
     return(
         <main>
@@ -26,14 +53,23 @@ function App() {
                 </div>
             </div>
             <div className={newOcorrencia ? "newOcorrencia abre" : "newOcorrencia fecha"}>
-                <form>
-                    <label htmlFor="">Titulo</label>
-                    <input name='titulo' type="text" id='titulo' />
+                <form className='container' onSubmit={handleSubmit(onSubmit)}>
+                    <div className='titleOcorrencia'>
+                        <label htmlFor="titulo">Titulo</label>
+                        <input  className='titleOcorrencias' type="text" id='titulo' {...register("title")} placeholder='Titulo'/>
+                    </div>
+                    <div className='descriptionOcorrencia' >
+                        <textarea className='descriptionOcorrencias' {...register("description")} placeholder='Descreva o ocorrido'></textarea>                       
+                    </div>
+                    <div className='inputOcorrencia' >
+                        <input className='inputOcorrencias' type="file" multiple {...register("fotos")}/>
+                    </div>
+                    <div className='enviaOcorrencia' >
+                        <button type='submit'>Enviar</button>
+                    </div>
                 </form>
             </div>
-        </main>
-        
-        
+        </main>       
     )
 }
 
